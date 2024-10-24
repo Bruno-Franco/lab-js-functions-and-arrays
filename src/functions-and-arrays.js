9e7162ae99cfb3b8809058e115fb9c272bfe82f1
Bruno Frasan <brunofrancco@gmail.com>
2024-10-24 15:22:38 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,84 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(n1, n2) {
+	return Math.max(n1, n2)
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+	'mystery',
+	'brother',
+	'aviator',
+	'crocodile',
+	'pearl',
+	'orchard',
+	'crackpot',
+]
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+	let biggest = ''
+	let temp = 0
+	if (arr.length === 0) {
+		return null
+	}
 
-
-
+	for (let i = 0; i < arr.length; i++) {
+		let letterNum = arr[i].length
+		if (letterNum > temp) {
+			temp = letterNum
+			biggest = arr[i]
+		}
+	}
+	return biggest
+}
 
 // Iteration 3 | Sum Numbers
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10]
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(arr) {
+	if (arr.length === 0) {
+		return 0
+	} else if (arr.length === 1) {
+		return arr[0]
+	} else {
+		let sum = 0
+		for (let i = 0; i < arr.length; i++) {
+			sum += arr[i]
+		}
+		return sum
+	}
+}
 
 // Iteration 4 | Numbers Average
-const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9]
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(arr) {
+	if (arr.length === 0) {
+		return 0
+	} else {
+		let avg = 0
+		for (let i = 0; i < arr.length; i++) {
+			avg += arr[i]
+		}
+		return avg / arr.length
+	}
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+	'machine',
+	'subset',
+	'trouble',
+	'starting',
+	'matter',
+	'eating',
+	'truth',
+	'disobedience',
+]
 
-function doesWordExist() {}
+function doesWordExist(arr, toFind) {
+	if (arr.length === 0) {
+		return null
+	} else {
+		return arr.includes(toFind)
+	}
+}
